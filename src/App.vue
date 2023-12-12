5d2f23507d9070a95778b0be2e6e1e50877d4424
MSI\user <s97011501@gmail.com>
2023-12-12 14:57:02 +0800

    feat: 新增初始 loading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useAreaStore } from './stores/area'
+import { useVotedataStore } from './stores/votedata'
 
 import Navbar from './components/Navbar.vue'
 import Filter from './components/Filter.vue'
@@ -8,12 +9,14 @@ import Overview from './components/Overview.vue'
 import Tips from './components/Tips.vue'
 import Result from './components/Result.vue'
 
-const store = useAreaStore()
+const areaStore = useAreaStore()
+const votedataStore = useVotedataStore()
+votedataStore.loadVotedata()
 </script>
 
 <template>
   <Navbar />
-  <div class="p-lg-6">
+  <div class="p-lg-6" v-if="!votedataStore.loading">
     <Filter />
     <div class="row g-0">
       <div class="col-lg-4">
@@ -23,10 +26,16 @@ const store = useAreaStore()
         <Map />
       </div>
       <div class="col-lg-4">
-        <Tips v-if="store.selectedCity === ''" />
+        <Tips v-if="areaStore.selectedCity === ''" />
         <Result v-else />
       </div>
     </div>
+  </div>
+  <div class="text-center mt-12" v-else>
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <div>本網站資料量較大，讀取中請稍後...</div>
   </div>
 </template>
 
--- src/stores/votedata.ts
+++ src/stores/votedata.ts
@@ -1,19 +1,45 @@
 import { defineStore } from 'pinia'
-import votedata from '../assets/json/votedata.json'
+// import votedata from '../assets/json/votedata.json'
+
+interface Votedata {
+  city: string,
+  dist: number,
+  vil: number,
+  candidate: number,
+  quantity: number,
+  percentage: number,
+  elected: string
+}
 
 export const useVotedataStore = defineStore('votedata', {
   state: () => ({ 
-    // 將省代碼與縣市代碼組合為新縣市代碼欄位
-    votedataList: votedata.map(item => {
-      const { province, city, ...others } = item
-      return {
-        city: `${province}${city}`,
-        ...others,
-      }
-    })
+    loading: true,
+    votedataList: [] as Array<Votedata>,
   }),
   getters: {
   },
   actions: {
+    async loadVotedata() {
+      try {
+        this.loading = true;
+
+        // 使用 import() 異步載入 JSON 檔案
+        const module = await import('../assets/json/votedata.json');
+
+        // 載入完成後,將省代碼與縣市代碼組合為新縣市代碼欄位,並將 loading 設置為 false
+        this.votedataList = module.default.map(item => {
+          const { province, city, ...others } = item;
+          return {
+            city: `${province}${city}`,
+            ...others,
+          };
+        });
+      } catch (error) {
+        console.error('Error loading votedata JSON file', error);
+        // 載入失敗時也將 loading 設置為 false
+      } finally {
+        this.loading = false;
+      }
+    },
   },
 })
